8c5f6961315958c34f5c5324e986b328d94d60a5
Ridhorzqullah <ridhorizqullah3@gmail.com>
2024-05-20 19:00:28 +0700

    class anak

--- aggregation/anak.h
+++ aggregation/anak.h
@@ -4,5 +4,15 @@
 using namespace std;
 #include <iostream>
 
-
+class anak {
+public:
+	string nama;
+	anak(string pNama) : nama(pNama) {
+		cout << "Anak \"" << nama << "\" ada\n";
+	}
+	~anak() {
+		cout << "Anak \"" << nama << "\"tidak ada\n";
+	}
+};
+#endif
 
