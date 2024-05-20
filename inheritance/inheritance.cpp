28f5f0883f23f31b4351a5f9f4d88f5e6f31c004
Ridhorzqullah <ridhorizqullah3@gmail.com>
2024-05-20 19:44:15 +0700

    int main inheritance

--- inheritance/inheritance.cpp
+++ inheritance/inheritance.cpp
@@ -36,3 +36,12 @@ public:
 
 };
 
+int main()
+{
+    pelajar siswa1("Andi Laksono", "SMAN 1 Bantul");
+    cout << siswa1.perkenalan();
+    cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
+
+    return 0;
+}
+
